aa52e7fee0a9c257ee2c1e7183c503160bb07f5d
kaijin <kaijinkaijin2@gmail.com>
2023-11-27 15:21:50 +0800

    app code init

--- gdsc/lib/main.dart
+++ gdsc/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -9,37 +11,50 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+
+
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State createState() => _MyAppState();
+
+
+  
+  // This widget is the root of your application.
+  
 }
+
+class _MyAppState extends State {
+        late GoogleMapController mapController;
+      
+        final LatLng _center = const LatLng(-33.86, 151.20);
+      
+        void _onMapCreated(GoogleMapController controller) {
+          mapController = controller;
+        }
+      
+        @override
+        Widget build(BuildContext context) {
+          return MaterialApp(
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text('Maps Sample App'),
+                backgroundColor: Colors.green[700],
+              ),
+              body: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+              ),
+            ),
+          );
+        }
+      }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -128,5 +143,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
