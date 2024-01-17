72b9059c0dcf4e58cc2e30d8ec6a8abf15954899
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-01-18 05:13:58 +0800

    Revamp main.dart
    -change app bar to a pill
    -add scrollable sheet for future locations

--- gdsc/lib/main.dart
+++ gdsc/lib/main.dart
@@ -6,136 +6,89 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name:"potent-result-406711",
+    name: "potent-result-406711",
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
   State createState() => _MyAppState();
-  
+
   // This widget is the root of your application.
-  
 }
 
 class _MyAppState extends State {
-        late GoogleMapController mapController;
-      
-        final LatLng _center = const LatLng(22.6239974, 120.2981408);
-      
-        void _onMapCreated(GoogleMapController controller) {
-          mapController = controller;
-        }
-      
-        @override
-        Widget build(BuildContext context) {
-          return MaterialApp(
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Text('Maps Sample App'),
-                backgroundColor: Colors.green[700],
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(22.6239974, 120.2981408);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+                borderRadius: BorderRadius.circular(15.0),
               ),
-              body: GoogleMap(
+              child: Text(
+                'App Name',
+                style: TextStyle(color: Colors.grey[800], fontSize: 20),
+              ),
+            ),
+          ),
+          body: Stack(
+            children: <Widget>[
+              GoogleMap(
                 onMapCreated: _onMapCreated,
                 initialCameraPosition: CameraPosition(
                   target: _center,
                   zoom: 12.5,
                 ),
+                myLocationButtonEnabled: false,
+                zoomControlsEnabled: false,
               ),
-            ),
-          );
-        }
-      }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+              DraggableScrollableSheet(
+                initialChildSize: 0.3,
+                minChildSize: 0.05,
+                maxChildSize: 0.9,
+                builder:
+                    (BuildContext context, ScrollController scrollController) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      // remove or decrease the borderRadius
+                      borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(20.0)),
+                    ),
+                    child: ListView.builder(
+                      controller: scrollController,
+                      itemCount: 25,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(title: Text('Item $index'));
+                      },
+                      padding: EdgeInsets.zero,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
