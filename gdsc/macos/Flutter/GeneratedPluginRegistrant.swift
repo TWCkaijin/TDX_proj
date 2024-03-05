01024bb6b53b655003f17d47024ed2706609a4f2
kaijin <kaijinkaijin2@gmail.com>
2024-03-05 21:30:22 +0800

    debug

--- gdsc/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gdsc/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_core
 import firebase_database
+import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
