cdc09cfbe1b3dda2b062b6c8824323d4f775ac36
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-03-27 21:57:47 +0800

    hide key for ios

--- gdsc/ios/Runner/AppDelegate.swift
+++ gdsc/ios/Runner/AppDelegate.swift
@@ -9,7 +9,13 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyANhrh7_1BgTMYur-9AzLugKB5eE26KnGY")
+    if let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+       let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject],
+       let apiKey = dict["googleMapsApiKey"] as? String {
+      GMSServices.provideAPIKey(apiKey)
+    } else {
+      fatalError("Failed to load Google Maps API key")
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
